126d5a7c82286a973c2cf2a7cedce4fd7041fe8b
reu <ryoiwanaga@gmail.com>
2016-07-31 07:22:15 +0900

    Add Observer pattern

---
+++ Observer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DP
+{
+    public interface IObserver
+    {
+        void Nortify();
+    }
+
+    public class Observable
+    {
+        List<IObserver> observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        protected void Nortify()
+        {
+            for (int i = 0; i < observers.Count; i++)
+            {
+                observers[i].Nortify();
+            }
+        }
+    }
+}
